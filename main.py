d4554066581827ff5b2ebc3e03940db08a48fd4f
ymotkpo3 <76632004+ymotkpo3@users.noreply.github.com>
2021-07-24 21:08:09 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+import re
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    mensaje1 = message.content.lower()
+    mensajeR = re.findall('cum', mensaje1)
+
+    if mensajeR:
+        await message.reply("lo que te encanta:sweat_drops:")
+
+client.run('ODMwNTM5NjQyOTY0Mjc5MzI4.YHIKUQ.1qr7Koqf4hnjGjkEEVpO526Napg')
+
